ba6db7c5fcfb42631a9f1224bee73692f805055b
Evan <eg396@njit.edu>
2025-06-23 16:28:08 -0400

    chore: add calculation factory class

--- app/calculation/__init__.py
+++ app/calculation/__init__.py
@@ -56,3 +56,65 @@ class Calculation(ABC):
         ## str: A string representation of the calculation
 
         return f"{self.__class__.__name__}(num1 = {self.num1}, num2 = {self.num2})"
+    
+## Factory class for calculations
+
+class CalculationFactory:
+
+    ## initialize a dictionary for mapping calculation types
+
+    _calculations = {}
+
+    @classmethod
+    def register_calculation(cls, type: str):
+
+        ## Registers a new calculation type.
+
+        ## Params:
+        ## type: str
+
+        ## Returns:
+        ## decorator: function call (which in turn gives us a registered subclass)
+
+        def decorator(subclass):
+
+            ## Decorator function to register a new calculation type.
+
+            ## Params:
+            ## subclass: A subclass of Calculation
+
+            ## Returns:
+            ## Subclass: The registered subclass
+
+            type_lower = type.lower()
+            if type_lower in cls._calculations:
+
+                raise ValueError (f"Calculation type {type_lower} is already registered.")
+            
+            cls._calculations[type_lower] = subclass
+            return subclass
+
+        return decorator
+    
+    @classmethod
+    def create_calculations(cls, type: str, num1: float, num2: float) -> Calculation:
+
+        ## Creates a new calculation instance.
+
+        ## Params:
+        ## type: str
+        ## num1: float
+        ## num2: float
+
+        ## Returns:
+        ## Calculation: A new instance of the specified calculation type
+
+        type_lower = type.lower()
+        calculation_class = cls._calculations.get(type_lower)
+
+        if not calculation_class:
+
+            available_types = ', '.join(cls._calculations.keys())
+            raise ValueError(f"Invalid calculation type: {type}. Available types: {available_types}")
+        
+        return calculation_class(num1, num2)
